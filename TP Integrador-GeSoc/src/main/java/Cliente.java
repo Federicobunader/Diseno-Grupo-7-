b7702d0ae57d95a5ba2a735391ca72171c7aa7b5
Juan Cruz Vacatello <JuanVacatello@users.noreply.github.com>
2020-04-10 18:49:25 -0300

    correccion de error

--- TP Integrador-GeSoc/src/main/java/Cliente.java
+++ TP Integrador-GeSoc/src/main/java/Cliente.java
@@ -1,10 +1,4 @@
 
-
-/**
- * @author Fede
- * @version 1.0
- * @created 08-abr-2020 05:50:23 p.m.
- */
 public class Cliente {
 
 	private char Nombre;
--- TP Integrador-GeSoc/src/main/java/Item.java
+++ TP Integrador-GeSoc/src/main/java/Item.java
@@ -3,11 +3,6 @@ public class Item {
 
 	private Producto Detalle;
 	private char Descripcion;
-	/**
-	 * <ol>
-	 * 	<li></li>
-	 * </ol>
-	 */
 	private int Cantidad;
 	public Producto m_Producto;
 
