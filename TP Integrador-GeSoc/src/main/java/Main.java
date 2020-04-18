bc8713efa93564902ea020ce62c4e1e975bf2680
Federicobunader <39320918+Federicobunader@users.noreply.github.com>
2020-04-18 20:01:03 -0300

    Singleton Sistema + metodo arrancar

--- TP Integrador-GeSoc/src/main/java/Main.java
+++ TP Integrador-GeSoc/src/main/java/Main.java
@@ -9,8 +9,8 @@ import java.security.NoSuchAlgorithmException;
 public class Main {
     public static void main(String[] args) {
 
-        GestorDeUsuarios gestorDeUsuarios = GestorDeUsuarios.GetInstance();
-        gestorDeUsuarios.consolaUsuario();
+        Sistema sistema = Sistema.GetInstance();
+        sistema.arrancar();
 
     }
 
--- TP Integrador-GeSoc/src/main/java/Sistema.java
+++ TP Integrador-GeSoc/src/main/java/Sistema.java
@@ -1,2 +1,26 @@
+
+
 public class Sistema {
+
+    GestorDePasswords gestorDePasswords = GestorDePasswords.GetInstance();
+
+    private static Sistema instance = null;
+
+    private Sistema() {
+    }
+
+    public static Sistema GetInstance() {
+        if (instance == null)
+            instance = new Sistema();
+        return instance;
+    }
+
+
+
+
+    public void arrancar(){
+        GestorDeUsuarios gestorDeUsuarios = GestorDeUsuarios.GetInstance();
+        gestorDeUsuarios.consolaUsuario();
+    }
+
 }
